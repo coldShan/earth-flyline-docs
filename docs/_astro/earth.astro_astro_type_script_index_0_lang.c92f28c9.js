3093fbcc55cc804b62c9078f4e78c0ca0df0485f
quentin.guan@tophant <quentin.guan@tophant.com>
2023-07-13 20:25:25 +0800

    build doc

---
+++ docs/_astro/earth.astro_astro_type_script_index_0_lang.c92f28c9.js
@@ -0,0 +1,1 @@
+import{e as o,w as r}from"./world.3f811676.js";o.registerMap("world",r);const e=document.getElementById("container");o.init({dom:e,map:"world",bgColor:"#16181d",config:{spriteColor:"#16181d"}});
