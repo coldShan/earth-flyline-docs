fd1cdb6b2fe078a0388bb7fe47460e1cddc8b882
quentin.guan@tophant <quentin.guan@tophant.com>
2023-07-13 20:15:40 +0800

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,51 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+    outDir: './docs',
+  integrations: [
+    starlight({
+      title: 'Earth-flyLine',
+      social: {
+        github: 'https://github.com/JavaScriptam/earth-flyLine',
+      },
+      sidebar: [
+        {
+          label: '起步',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: '介绍', link: '/guides/start/' },
+            { label: '安装', link: '/guides/install/' },
+          ],
+        },
+        {
+          label: '文档',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: '示例', link: '/reference/example/' },
+            { label: '参数', link: '/reference/property/' },
+            { label: '方法', link: '/reference/methods/' },
+          ],
+        },
+      ],
+      defaultLocale: 'zh',
+      locales: {
+        // English docs in `src/content/docs/en/`
+        // en: {
+        //   label: 'English',
+        // },
+        // Simplified Chinese docs in `src/content/docs/zh/`
+        zh: {
+          label: '简体中文',
+          lang: 'zh-CN',
+        },
+        // Arabic docs in `src/content/docs/ar/`
+        
+      },
+    }),
+  ],
+
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
---
+++ src/content/docs/guides/start.md
@@ -0,0 +1,16 @@
+---
+title: 介绍
+
+---
+
+## ✨ 特性
+
+    📦 开箱即用：简单快速集成，基本零成本上手。
+
+    🛡 Typescript开发：提供完整的类型定义。
+
+    🎨 高可扩展：丰富的样式配置和API。
+
+## 🤔 反馈
+
+使用过程中的问题或者建议欢迎提 [issue](https://github.com/JavaScriptam/earth-flyLine/issues) ，如果该项目为您提供了帮助希望来一个star万分感谢。
