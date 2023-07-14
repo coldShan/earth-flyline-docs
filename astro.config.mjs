2f5ac397873882319f14f9a026373208a4e32a69
quentin.guan@tophant <quentin.guan@tophant.com>
2023-07-14 10:25:24 +0800

    udpate

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    outDir: './docs',
     site:'https://javascriptam.github.io/earth-flyline-docs/',
     integrations: [
       starlight({
